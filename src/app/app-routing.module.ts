f05a501d98f84ed81396100153b1d21ba7c97d4e
Sebastien DErrico <sebastien@hollox.net>
2020-05-14 20:20:46 -0400

    Protect profile page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {ProfileComponent} from "./profile/profile.component";
+import {AuthGuard} from "./auth/auth.guard";
 
 
 const routes: Routes = [
   {
     path: 'profile',
-    component: ProfileComponent
+    component: ProfileComponent,
+    canActivate: [AuthGuard]
   }
 ];
 
