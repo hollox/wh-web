c5f5bdf71f68f91b57e0a68686bd66833a8ac8ad
Sebastien DErrico <sebastien@hollox.net>
2020-05-17 08:08:46 -0400

    Add validation to name fields

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -44,7 +45,8 @@ import { PageOrganizationListComponent } from '../pages/organizations/list/page-
     BrowserAnimationsModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
     HttpClientModule,
-    MaterialModule
+    MaterialModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/pages/organizations/detail/page-organization-detail.component.ts
+++ src/pages/organizations/detail/page-organization-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {OrganizationsService} from "../../../shared/organizations/organizations.service";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-page-organization-detail',
@@ -9,19 +10,19 @@ import {OrganizationsService} from "../../../shared/organizations/organizations.
 })
 export class PageOrganizationDetailComponent implements OnInit {
 
-  // organizationFormGroup: FormGroup;
+  organizationFormGroup: FormGroup;
 
   constructor(public route: ActivatedRoute, public organizationService: OrganizationsService) { }
 
   ngOnInit(): void {
-    // this.organizationFormGroup = new FormGroup({
-    //   id: new FormControl(),
-    //   name: new FormControl(null, [Validators.required]),
-    // });
+    this.organizationFormGroup = new FormGroup({
+      id: new FormControl(),
+      name: new FormControl(null, [Validators.required]),
+    });
   }
 
   onSubmit(): void {
-    // console.log(this.organizationFormGroup);
+    console.log(this.organizationFormGroup);
     // this.organizationFormGroup.reset();
   }
 }
